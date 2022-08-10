11f8c7015a746512e3402e3fd41ab145d14b9bb8
Diego Luna <103113803+DiegoMoon@users.noreply.github.com>
2022-08-10 18:12:14 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
-import test1.py from *
+lugar = str(input("Ingresa tu lugar de nacimiento: "))
+edad = int (input("Ingresa tu edad:  "))
 
-if (edad >= 18)
-  print("Esta persona puede votar")
-else
-  print("Esta persona NO puede votar")
+if(edad >= 18):
+    print(f'La persona con {edad} años puede votar en {lugar}')
+else:
+    print("Esta persona no tiene edad suficiente para votar")
